9061d38804dd6e84b9e11c9fd40021000956fd06
Давид <david@192.168.1.3>
2024-11-20 16:04:23 +0300

    made homework_1

--- project/utils/get_ordered_products_by_price.py
+++ project/utils/get_ordered_products_by_price.py
@@ -1,2 +1,5 @@
-def get_ordered_products_by_price(products):
-    pass
+from models.product import Product
+
+
+def get_ordered_products_by_price(products: list[Product]) -> list[Product]:
+    return sorted(products, key=lambda product: product.price, reverse=True)
