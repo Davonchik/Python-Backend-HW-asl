0a69d74d920f321e25056d6c99e93aaea3230ee5
David Aslanian <97387580+Davonchik@users.noreply.github.com>
2024-12-10 15:10:58 +0300

    Delete home_work_1 directory

--- home_work_1/project/utils/extract_prices.py
+++
@@ -1,5 +0,0 @@
-from models.product import Product
-
-
-def extract_prices(products: list[Product]) -> list[int]:
-    return [product.price for product in products]
--- home_work_1/project/utils/select_products_by_category.py
+++
@@ -1,5 +0,0 @@
-from models.product import Product
-
-
-def select_products_by_category(products: list[Product], category: str) -> list[Product]:
-    return [product for product in products if product.category == category]
